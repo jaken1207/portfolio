6b6b66dad5d67bdca3ed59517741120a5c605bd4
naga <yuto7687@gmail.com>
2023-01-28 17:47:18 +0900

    ✨ Heroコンポーネントを追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,20 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '@/components/Header'
+import Hero from '@/components/Hero'
 
 export default function Home() {
   return (
-    <>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
       <Head>
         <title>Portfolio</title>
       </Head>
   
     <Header />
 
-    {/* Hero */}
+    <section id='hero' className='snap-center'>
+      <Hero />
+    </section>
 
     {/* About */}
 
@@ -22,6 +25,6 @@ export default function Home() {
     {/* Peoject */}
 
     {/* Contact Me */}
-    </>
+    </div>
   )
 }
