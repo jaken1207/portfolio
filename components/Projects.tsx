2b4e9e689afa1d9ac5622b8d9bb6c3543588d41f
naga <yuto7687@gmail.com>
2023-01-30 02:21:10 +0900

    🐛 map関数にkeyを与えた

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -16,8 +16,8 @@ export default function Projects({}: Props) {
         </h3>
 
         <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20'>
-            {projects.map((project, i, key=i) => (
-                <div className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
+            {projects.map((project, i) => (
+                <div key={ projects.length }className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
                     <motion.img 
                     initial={{
                         y: -300,
@@ -29,7 +29,7 @@ export default function Projects({}: Props) {
                     src="https://cdn.sanity.io/images/ltuexkre/production/af7ca99b5a796d0698cf9121a4a0795b5022b6be-666x375.png" alt="" />
                     <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
                         <h4 className='text-4xl font-semibold text-center'>
-                            <span className='underline decoration-[#F7AB0A]/50'>Case Study {i+1} of {projects.length} </span>: UPS clone 
+                            <span className='underline decoration-[#F7AB0A]/50'>Case Study {i+1} of {projects.length}</span>: UPS clone 
                         </h4>
                         <p className='text-lg text-center md:text-left'>
                             aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
