ed418ccae5c6d5e576407c8f10cddf631d6550f0
naga <yuto7687@gmail.com>
2023-01-30 00:17:42 +0900

    ✨ Aboutコンポーネントの追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Header from '@/components/Header'
 import Hero from '@/components/Hero'
+import About from '@/components/About'
 
 export default function Home() {
   return (
@@ -16,7 +17,9 @@ export default function Home() {
       <Hero />
     </section>
 
-    {/* About */}
+    <section id= "about" className='snap-center'>
+      <About />
+    </section>
 
     {/* Experience */}
 
