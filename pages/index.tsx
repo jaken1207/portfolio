3023df7c889fff48f87602ccf15c4ee03dd1bfe4
naga <yuto7687@gmail.com>
2023-03-02 02:01:42 +0900

    ✏️ 多少の文言を修正しました

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,58 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Header from '@/components/Header'
-import Hero from '@/components/Hero'
-import About from '@/components/About'
-import WorkExperience from '@/components/WorkExperience'
-import Skills from '@/components/Skills'
-import Projects from '@/components/Projects'
-import ContactMe from '@/components/ContactMe'
-import Link from 'next/link'
+import Head from "next/head";
+import Image from "next/image";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import WorkExperience from "@/components/WorkExperience";
+import Skills from "@/components/Skills";
+import Projects from "@/components/Projects";
+import ContactMe from "@/components/ContactMe";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
       <Head>
         <title>Portfolio</title>
       </Head>
-  
-    <Header />
 
-    <section id='hero' className='snap-center'>
-      <Hero />
-    </section>
+      <Header />
 
-    <section id= "about" className='snap-center'>
-      <About />
-    </section>
+      <section id="hero" className="snap-center">
+        <Hero />
+      </section>
 
-    <section id="experience" className='snap-center'>
-      <WorkExperience />
-    </section>
+      <section id="about" className="snap-center">
+        <About />
+      </section>
 
-    <section id='skills' className='snap-start'>
+      <section id="experience" className="snap-center">
+        <WorkExperience />
+      </section>
+
+      <section id="skills" className="snap-start">
         <Skills />
       </section>
 
-    <section id='projects' className='snap-start'>
-      <Projects />
-    </section>
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
 
-    <section id='contact' className='snap-start'>
-      <ContactMe />
-    </section>
+      <section id="contact" className="snap-start">
+        <ContactMe />
+      </section>
 
       <Link href="#hero">
-          <footer className='sticky bottom-5 w-full cursor-pointer'>
-            <div className='flex items-center justify-center'>
-              <img className='h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer' src="https://i.imgur.com/e2yvD6A.png" alt="" />
-            </div>
-          </footer>
+        <footer className="sticky bottom-5 w-full cursor-pointer">
+          <div className="flex items-center justify-center">
+            <img
+              className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
+              src="/yuto.png"
+              alt=""
+            />
+          </div>
+        </footer>
       </Link>
     </div>
-  )
+  );
 }
