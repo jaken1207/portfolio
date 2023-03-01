ad33602c275b8b6656d47cb11df86c182e4a7ce9
naga <yuto7687@gmail.com>
2023-03-01 17:54:57 +0900

    ⚗️ vercelのpublicの扱い方

--- components/About.tsx
+++ components/About.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { motion } from "framer-motion";
-import nagata from "../public";
 
 type Props = {};
 
@@ -27,7 +26,7 @@ export default function About({}: Props) {
         whileInView={{ opacity: 1, x: 0 }}
         viewport={{ once: true }}
         className="-md-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
-        src="nagata.png"
+        src="/nagata.png"
       />
       <div className="space-y-10 px-0 md:px-10">
         <h4 className="text-4xl font-semibold">
