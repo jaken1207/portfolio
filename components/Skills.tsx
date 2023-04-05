9a1029faf2d9c83cafdb49619724f7dbd49ea8fe
naga <yuto7687@gmail.com>
2023-04-05 12:41:16 +0900

    ✏️ 文字修正

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -31,7 +31,7 @@ export default function Skills({}: Props) {
       </h3>
 
       <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm">
-        Hover over a skill for currency profieciecy
+        Hover over a skill for currency profieciency
       </h3>
 
       <div className="grid grid-cols-4 gap-5">
